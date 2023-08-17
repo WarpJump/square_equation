ef7c06732f21d2c73e9da5656be0716c966ed3c6
Danila Gushchin <warpjump@bk.ru>
2023-08-17 13:15:06 +0300

    adding fixes

--- square_eq.cpp
+++ square_eq.cpp
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <stdio.h>
 
+#include <cassert>
+
 /* program that takes three floating-point numbers and solves square equation
  * assuming them as coefficients */
 
@@ -8,58 +10,69 @@ static const double kEpsilonLocality = 0.0001;
 
 static const int kInfiniteRoots = 10;
 
-inline int IsZero(double value) { return fabs(value) < kEpsilonLocality; }
+enum Roots { Zero, One, Two, Inf, NaR };
 
-int RootOfLinearEquation(double b_coef, double c_coef, double* root);
+inline int IsZero(double value) { return (fabs(value) < kEpsilonLocality); }
 
-int RootsOfSquareEquation(double a_coef, double b_coef, double c_coef,
-                          double* root_1, double* root_2);
+void ScanCoeffs(double* a_coef, double* b_coef, double* c_coef);
+
+Roots RootOfLinearEquation(double b_coef, double c_coef, double* root);
+
+Roots RootsOfSquareEquation(double a_coef, double b_coef, double c_coef,
+                            double* root_1, double* root_2);
+
+void PrintRoots(Roots num_of_roots, double root_1, double root_2);
 
 int main() {
-  int num_of_roots;
-  double a_coef;
-  double b_coef;
-  double c_coef;
-  double root_1;
-  double root_2;
-  scanf("%lf", &a_coef);
-  scanf("%lf", &b_coef);
-  scanf("%lf", &c_coef);
+  Roots num_of_roots = NaR;
+  double a_coef = NAN;
+  double b_coef = NAN;
+  double c_coef = NAN;
+  double root_1 = 0;
+  double root_2 = 0;
+
+  ScanCoeffs(&a_coef, &b_coef, &c_coef);
 
   num_of_roots =
       RootsOfSquareEquation(a_coef, b_coef, c_coef, &root_1, &root_2);
 
-  if (num_of_roots == kInfiniteRoots) {
-    printf("infinite roots exist\n");
-    return 0;
-  }
+  PrintRoots(num_of_roots, root_1, root_2);
 
-  if (num_of_roots == 2) {
-    printf("two roots exist: %3.3lg and %3.3lg \n", root_1, root_2);
-    return 0;
-  }
-
-  if (num_of_roots == 1) {
-    printf("one root exist: %3.3lg \n", root_1);
-    return 0;
-  }
-  printf("no roots exist\n");
   return 0;
 }
 
-int RootOfLinearEquation(double b_coef, double c_coef, double* root) {
-  if (IsZero(b_coef)) {
-    if (IsZero(c_coef)) {
-      return kInfiniteRoots;
-    }
-    return 0;
-  }
-  *root = -c_coef / b_coef;
-  return 1;
+void ScanCoeffs(double* a_coef, double* b_coef, double* c_coef) {
+  assert(a_coef != b_coef && b_coef != c_coef && a_coef != b_coef);
+  assert(a_coef != nullptr);
+  assert(b_coef != nullptr);
+  assert(c_coef != nullptr);
+
+  scanf("%lf", a_coef);
+  scanf("%lf", b_coef);
+  scanf("%lf", c_coef);
 }
 
-int RootsOfSquareEquation(double a_coef, double b_coef, double c_coef,
-                          double* root_1, double* root_2) {
+Roots RootOfLinearEquation(double b_coef, double c_coef, double* root) {
+  assert(root != nullptr);
+
+  if (IsZero(b_coef)) {
+    if (IsZero(c_coef)) {
+      return Inf;
+    }
+    return Zero;
+  }
+  *root = -c_coef / b_coef;
+  return One;
+}
+
+Roots RootsOfSquareEquation(double a_coef, double b_coef, double c_coef,
+                            double* root_1, double* root_2) {
+  assert(!isnan(a_coef) && !isnan(b_coef) && !isnan(c_coef));
+
+  assert(root_2 != root_1);
+  assert(root_1 != nullptr);
+  assert(root_2 != nullptr);
+
   if (IsZero(a_coef)) {
     return RootOfLinearEquation(b_coef, c_coef, root_1);
   }
@@ -67,12 +80,39 @@ int RootsOfSquareEquation(double a_coef, double b_coef, double c_coef,
   double disc = b_coef * b_coef - 4 * a_coef * c_coef;
   if (IsZero(disc)) {
     *root_1 = -b_coef / (2 * a_coef);
-    return 1;
+    return One;
   }
   if (disc < 0) {
-    return 0;
+    return Zero;
   }
   *root_1 = (-b_coef + sqrt(disc)) / (2 * a_coef);
   *root_2 = (-b_coef - sqrt(disc)) / (2 * a_coef);
-  return 2;
+  return Two;
+}
+
+void PrintRoots(Roots num_of_roots, double root_1, double root_2) {
+  switch (num_of_roots) {
+    case One:
+      printf("one root exist: %3.3lg \n", root_1);
+      break;
+
+    case Two:
+      printf("two roots exist: %3.3lg and %3.3lg \n", root_1, root_2);
+      break;
+
+    case Inf:
+      printf("infinite roots exist\n");
+      break;
+
+    case Zero:
+      printf("no roots exist\n");
+      break;
+
+    case NaR:
+      printf("error\n");
+      break;
+
+    default:
+      printf("error\n");
+  }
 }
