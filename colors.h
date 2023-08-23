b6a70495aee216dcdadcdc39402165b3ac38cf8d
Danila Gushchin <warpjump@bk.ru>
2023-08-23 21:22:19 +0300

    separating visual text function

---
+++ colors.h
@@ -0,0 +1,9 @@
+/*!
+\file 
+\brief Header file containing visual text macroses
+*/
+
+/*!
+  \brief Macros that paints text in branches in red
+*/
+#define RedText(text) "\033[0;31m" text "\033[0m"
